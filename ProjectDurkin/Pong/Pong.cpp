671262f7b1d31e3b3281b13426aa6265f4fd24bb
Arthur <arajala@umich.edu>
2015-11-10 08:57:42 -0500

    pong driver implementation

---
+++ ProjectDurkin/Pong/Pong.cpp
@@ -0,0 +1,286 @@
+#include "Pong.hpp"
+
+using namespace std;
+
+bool Pong::connectPlayer1(int port) {
+
+	struct sockaddr_in serv_addr;
+	struct hostent* server;
+
+	// Create the socket
+	player_1_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (player_1_fd < 0) {
+		cout << "E: Failed to create socket" << endl;
+		return false
+	}
+
+	// Populate server information
+	server = gethostbyname("localhost");
+	if (server == NULL) {
+		cout << "E: Failed to open socket on localhost" << endl;
+		return false;
+	}
+	bzero((char*)&serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(port);
+
+	// Connect
+	if (connect(player_1_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+		cout << "E: Failed to connect" << endl;
+		return false;
+	}
+
+	return true;
+}
+
+bool Pong::connectPlayer2(int port) {
+
+	struct sockaddr_in serv_addr;
+	struct hostent* server;
+
+	// Create the socket
+	player_2_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (player_2_fd < 0) {
+		cout << "E: Failed to create socket" << endl;
+		return false;
+	}
+
+	// Populate server information
+	server = gethostbyname("localhost");
+	if (server == NULL) {
+		cout << "E: Failed to open socket on localhost" << endl;
+		return false;
+	}
+	bzero((char*)&serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(port);
+
+	// Connect
+	if (connect(player_2_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+		cout << "E: Failed to connect" << endl;
+		return false;
+	}
+}
+
+void Pong::sendPlayer1Packet() {
+
+	// Populate the connect test buffer
+	char send_buf[16];
+	char recv_buf[1];
+	send_buf[0] = 'T';
+
+	// Test connection
+	int n_bytes = write(player_1_fd, send_buf, 1);
+	if (n_bytes < 0) {
+		cout << "E: Failed test write, player 1 disconnected" << endl;
+		exit(EXIT_FAILURE);
+	}
+	n_bytes = read(player_1_fd, recv_buf, 1);
+	if (recv_buf[0] != 'P') {
+		cout << "E: Failed test read, player 1 disconnected" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// Prepare trimmed data
+	unsigned char tr_p1_score = (unsigned char)player_1_score;
+	unsigned char tr_p2_score = (unsigned char)player_2_score;
+	unsigned short tr_ball_x = (unsigned short)ball_x_position;
+	unsigned short tr_ball_y = (unsigned short)ball_y_position;
+	unsigned short tr_p1_pos = (unsigned short)player_1_position;
+	unsigned short tr_p2_pos = (unsigned short)player_2_position;
+	unsigned char tr_p1_hit = (unsigned char)player_1_paddle_hit;
+	player_1_paddle_hit = false;
+
+	// Populate the data buffer
+	send_buf[0] = (char)tr_p1_score;
+	send_buf[1] = (char)tr_p2_score;
+	send_buf[2] = (char)(tr_ball_x & 0xFF);
+	send_buf[3] = (char)((tr_ball_x >> 8) & 0xFF);
+	send_buf[4] = (char)(tr_ball_y & 0xFF);
+	send_buf[5] = (char)((tr_ball_y >> 8) & 0xFF);
+	send_buf[6] = (char)(tr_p1_pos & 0xFF);
+	send_buf[7] = (char)((tr_p1_pos >> 8) & 0xFF);
+	send_buf[8] = (char)(tr_p2_pos & 0xFF);
+	send_buf[9] = (char)((tr_p2_pos >> 8) & 0xFF);
+	send_buf[10] = (char)(tr_p1_hit);
+
+	// Send the data
+	n_bytes = write(player_1_fd, send_buf, strlen(send_buf));
+	if (n_bytes < 0) {
+		cout << "E: Failed data write, player 1 disconnected" << endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Pong::sendPlayer2Packet() {
+
+	// Populate the connect test buffer
+	char send_buf[16];
+	char recv_buf[1];
+	send_buf[0] = 'T';
+
+	// Test connection
+	int n_bytes = write(player_2_fd, send_buf, 1);
+	if (n_bytes < 0) {
+		cout << "E: Failed test write, player 2 disconnected" << endl;
+		exit(EXIT_FAILURE);
+	}
+	n_bytes = read(player_2_fd, recv_buf, 1);
+	if (recv_buf[0] != 'P') {
+		cout << "E: Failed test read, player 2 disconnected" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// Prepare trimmed data
+	unsigned char tr_p1_score = (unsigned char)player_1_score;
+	unsigned char tr_p2_score = (unsigned char)player_2_score;
+	unsigned short tr_ball_x = (unsigned short)ball_x_position;
+	unsigned short tr_ball_y = (unsigned short)ball_y_position;
+	unsigned short tr_p1_pos = (unsigned short)player_1_position;
+	unsigned short tr_p2_pos = (unsigned short)player_2_position;
+	unsigned char tr_p2_hit = (unsigned char)player_2_paddle_hit;
+	player_2_paddle_hit = false;
+
+	// Populate the data buffer
+	send_buf[0] = (char)tr_p1_score;
+	send_buf[1] = (char)tr_p2_score;
+	send_buf[2] = (char)(tr_ball_x & 0xFF);
+	send_buf[3] = (char)((tr_ball_x >> 8) & 0xFF);
+	send_buf[4] = (char)(tr_ball_y & 0xFF);
+	send_buf[5] = (char)((tr_ball_y >> 8) & 0xFF);
+	send_buf[6] = (char)(tr_p1_pos & 0xFF);
+	send_buf[7] = (char)((tr_p1_pos >> 8) & 0xFF);
+	send_buf[8] = (char)(tr_p2_pos & 0xFF);
+	send_buf[9] = (char)((tr_p2_pos >> 8) & 0xFF);
+	send_buf[10] = (char)(tr_p2_hit);
+
+	// Send the data
+	n_bytes = write(player_2_fd, send_buf, strlen(send_buf));
+	if (n_bytes < 0) {
+		cout << "E: Failed data write, player 2 disconnected" << endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Pong::update() {
+
+	static bool first_move = true;
+	static double ball_angle = 0.0;
+	int new_x, new_y;
+
+	// Initialize the angle on start
+	if (first_move) {
+		random_device rd;
+		mt19937 gen(rd());
+		uniform_real_distribution<> dis(-45.0, 45.0);
+		ball_angle = dis(gen);
+		ball_x_position = (int)((double)BOARD_WIDTH / 2.0);
+	}
+
+	// Calculate a new position
+	new_x = (int)((double)ball_x_position + (double)BALL_SPEED * (double)cos(ball_angle));
+	new_y = (int)((double)ball_y_position + (double)BALL_SPEED * (double)sin(ball_angle));
+
+	// Check new position for bounces
+	int y_diff = 0;
+	if (new_y < 0) {
+		y_diff = 0 - new_y;
+		new_y = 0 + y_diff;
+		ball_angle = -1.0 * ball_angle;
+	} else if (new_y >= BOARD_HEIGHT) {
+		y_diff = new_y - BOARD_HEIGHT;
+		new_y = BOARD_HEIGHT - y_diff;
+		ball_angle = -1.0 * ball_angle;
+	}
+
+	// Check new position for scores
+	if (new_x < 0) {
+		if ((new_y >= player_1_position - PADDLE_RADIUS) &&
+			(new_y <= player_1_position + PADDLE_RADIUS)) {
+			if (ball_angle > 0) {
+				double axis_diff = ball_angle - 90.0;
+				ball_angle = 90.0 - axis_diff;
+			} else {
+				double axis_diff = -ball_angle - 90.0;
+				ball_angle = -90.0 + axis_diff;
+			}
+			int x_diff = 0 - new_x;
+			new_x = 0 + x_diff;
+			player_1_paddle_hit = true;
+		} else {
+			first_move = true;
+			player_2_score += 1;
+			new_x = BOARD_WIDTH / 2.0;
+			new_y = BOARD_HEIGHT / 2.0;
+		}
+	} else if (new_x >= BOARD_WIDTH) {
+		if ((new_y >= player_2_position - PADDLE_RADIUS) &&
+			(new_y <= player_2_position + PADDLE_RADIUS)) {
+			if (ball_angle > 0) {
+				double axis_diff = ball_angle - 0.0;
+			} else {
+
+			}
+			int x_diff = new_x - BOARD_WIDTH;
+			new_x = BOARD_WIDTH - x_diff;
+			player_2_paddle_hit = true;
+		} else {
+			first_move = true;
+			player_1_score += 1;
+			new_x = BOARD_WIDTH / 2.0;
+			new_y = BOARD_HEIGHT / 2.0;
+		}
+	}
+
+	// Update new position
+	ball_x_position = new_x;
+	ball_y_position = new_y;
+}
+
+bool Pong::isFinished() {
+	return (player_1_score >= FINAL_SCORE) || (player_2_score >= FINAL_SCORE);
+}
+
+int main(int argc, char** argv) {
+
+	// Check input
+	if (argc != 3) {
+		cout << "E: Must specify player 1 port and player 2 port" << endl;
+		exit(EXIT_FAILURE);
+	}
+	int player_1_port = atoi(argv[1]);
+	int player_2_port = atoi(argv[2]);
+
+	// Create pong game
+	Pong game;
+
+	// Connect player 1
+	if (!game.connectPlayer1(player_1_port)) {
+		cout << "E: Failed to connect to player 1 on port " << player_1_port << endl;
+		exit(EXIT_FAILURE);
+	}
+	cout << "PLAYER 1 CONNECTED" << endl;
+
+	// Connect player 2
+	if (!game.connectPlayer2(player_2_port)) {
+		cout << "E: Failed to connect to player 2 on port " << player_2_port << endl;
+		exit(EXIT_FAILURE);
+	}
+	cout << "PLAYER 2 CONNECTED" << endl;
+
+	// Start the game
+	cout << "GAME STARTING IN 3 SECONDS" << endl;
+	Sleep(3000);
+	while (!game.isFinished()) {
+		game.update();
+		game.sendPlayer1Packet();
+		game.sendPlayer2Packet();
+		Sleep(100);
+	}
+
+	cout << "GAME OVER" << endl;
+
+	return 0;
+}
---
+++ ProjectDurkin/Pong/Pong.hpp
@@ -0,0 +1,55 @@
+#ifndef PONG_HPP
+#define PONG_HPP
+
+#define BOARD_HEIGHT	256
+#define BOARD_WIDTH		512
+#define PADDLE_RADIUS	10
+#define BALL_RADIUS		2
+#define BALL_SPEED		5
+#define FINAL_SCORE		10
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <random>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+class Pong {
+
+public:
+
+	bool connectPlayer1(int port);
+
+	bool connectPlayer2(int port);
+
+	void sendPlayer1Packet();
+
+	void sendPlayer2Packet();
+
+	void update();
+
+	bool isFinished();
+
+private:
+
+	int player_1_fd;
+	int player_2_fd;
+
+	int ball_x_position;
+	int ball_y_position;
+
+	int player_1_position;
+	int player_2_position;
+
+	int player_1_score;
+	int player_2_score;
+
+	bool player_1_paddle_hit;
+	bool player_2_paddle_hit;
+
+};
+
+#endif
